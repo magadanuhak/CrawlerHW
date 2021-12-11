38bf86a09fa95c66dae752ae888b6d4f03ed179a
Daniel Maga - HOME WORK <magadanuhak@gmail.com>
2021-12-11 05:55:04 -0800

    Vithout blade

---
+++ example-app/app/Http/Controllers/MaklerCrawlerController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\MaklerCrawlerService;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+
+class MaklerCrawlerController extends Controller
+{
+    public function index(MaklerCrawlerService $service): Factory|View|Application
+    {
+        $shortDescriptions = $service->getShortDescriptions();
+
+        return view('short_articles', ['short_descriptions' => $shortDescriptions]);
+    }
+
+    public function fullDescriptions(MaklerCrawlerService $service): Factory|View|Application
+    {
+        $fullDescriptions = $service->getFullDescriptions();
+
+        return view('full_articles', ['full_descriptions' => $fullDescriptions]);
+    }
+}
+
